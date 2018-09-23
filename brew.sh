082f15d0c7333fe794cb29ecb4cfc599c3ba4d5a
Bassam Khouri <bassam.khouri@cenx.com>
2018-09-22 20:05:52 -0400

    Install brew bash and wget.  Don't pull repo in bootstrap

--- brew.sh
+++ brew.sh
@@ -27,17 +27,17 @@ brew install moreutils
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
-#brew install bash
+brew install bash
 brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
+#if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+#  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+#  chsh -s /usr/local/bin/bash;
+#fi;
 
 # Install `wget` with IRI support.
-#brew install wget --with-iri
+brew install wget --with-iri
 
 # Install GnuPG to enable PGP-signing commits.
 #brew install gnupg
