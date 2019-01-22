7eb750f5d90d2fbdae49a81c32af2718dfe73cbb
Bassam Khouri <bassam.khouri@cenx.com>
2019-01-22 08:52:35 -0500

    update bootstrap.sh - convert TAB to 4 spaces - show that NO is the default on the confirmation prompt

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,25 +5,25 @@ cd "$(dirname "${BASH_SOURCE}")";
 #git pull origin master > /dev/null 2>&1
 
 function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		--exclude ".gitignore" \
-		--exclude ".idea" \
-		-avh --no-perms . ~;
-	#source ${HOME}/.bash_profile;
+    rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude ".osx" \
+        --exclude "bootstrap.sh" \
+        --exclude "README.md" \
+        --exclude "LICENSE-MIT.txt" \
+        --exclude ".gitignore" \
+        --exclude ".idea" \
+        -avh --no-perms . ~;
+    #source ${HOME}/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+    doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+    read -p "This may overwrite existing files in your home directory. Are you sure? [y/N] " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
 fi;
 unset doIt;
