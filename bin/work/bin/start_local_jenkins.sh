172cfa042fa340b8583e13484b5834226202e085
Bassam Khouri <bassam.khouri@gmail.com>
2020-04-03 13:55:28 -0400

    Add a work script to start a local jenkins instance using docker

---
+++ bin/work/bin/start_local_jenkins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+DOCKER_IMAGE=jenkins/jenkins:lts
+DOCKER_CONTAINER_NAME=jenkins
+
+LOCAL_JENKINS_VOLUME=${HOME}/Documents/jenkins_home
+
+mkdir -p "${LOCAL_JENKINS_VOLUME}"
+
+set -x
+docker pull "${DOCKER_IMAGE}"
+docker run  --rm --name "${DOCKER_CONTAINER_NAME}" \
+            --publish 8080:8080 \
+            --publish 50000:50000 \
+            --network host
+            --volume "${LOCAL_JENKINS_VOLUME}":/var/jenkins_home \
+           "${DOCKER_IMAGE}"
+
