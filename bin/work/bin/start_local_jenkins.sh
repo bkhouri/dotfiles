bc3cc756d9bd20582985ce08207e1af3f43b138d
Bassam Khouri <bassam.khouri@gmail.com>
2020-10-14 11:09:15 -0400

    Do not use '--network host' when starting local jenkins instance

--- bin/work/bin/start_local_jenkins.sh
+++ bin/work/bin/start_local_jenkins.sh
@@ -11,7 +11,6 @@ docker pull "${DOCKER_IMAGE}"
 docker run  --rm --name "${DOCKER_CONTAINER_NAME}" \
             --publish 8080:8080 \
             --publish 50000:50000 \
-            --network host \
             --volume "${LOCAL_JENKINS_VOLUME}":/var/jenkins_home \
            "${DOCKER_IMAGE}"
 
