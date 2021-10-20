33007d64d3bd27b46e77d98c2abb7c681668064d
Bassam Khouri <bassam.khouri@gmail.com>
2021-10-20 09:34:27 -0400

    brew install gitui

--- brew-exclude.sh
+++ brew-exclude.sh
@@ -126,6 +126,7 @@ brew install git
 brew install git-lfs
 brew install git-review # automates and streamlines some of the tasks involved with submitting local changes to a Gerrit server for review
 brew install git-gui    # gitk and possibly other git gui tools
+brew install gitui      # https://github.com/extrawurst/gitui
 
 # Install gitbatch --> Ref: https://github.com/isacikgoz/gitbatch
 brew tap isacikgoz/taps
