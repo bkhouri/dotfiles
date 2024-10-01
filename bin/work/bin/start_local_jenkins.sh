fb27adddc40a58ad852730684774a24216a91138
Bassam Khouri <bkhouri@apple.com>
2024-10-01 09:10:23 -0400

    update start local jenkins script to use vessel as preferred engine

--- bin/work/bin/start_local_jenkins.sh
+++ bin/work/bin/start_local_jenkins.sh
@@ -1,14 +1,20 @@
-#!/bin/bash -e
+#!/bin/bash
 DOCKER_IMAGE=jenkins/jenkins:lts
 DOCKER_CONTAINER_NAME=jenkins
 
 LOCAL_JENKINS_VOLUME=${HOME}/Documents/jenkins_home
-
+preferred_container_engine=vessel
+preferred_cmd=$(command -v ${preferred_container_engine})
+container_cmd=$(command -v docker)
+if [[ -n "${preferred_cmd}" ]] ; then
+    container_cmd=$preferred_cmd
+fi
+set -e
 mkdir -p "${LOCAL_JENKINS_VOLUME}"
 
 set -x
-docker pull "${DOCKER_IMAGE}"
-docker run  --rm --name "${DOCKER_CONTAINER_NAME}" \
+$container_cmd pull "${DOCKER_IMAGE}"
+$container_cmd run  --rm --name "${DOCKER_CONTAINER_NAME}" \
             --publish 8080:8080 \
             --publish 50000:50000 \
             --volume "${LOCAL_JENKINS_VOLUME}":/var/jenkins_home \
